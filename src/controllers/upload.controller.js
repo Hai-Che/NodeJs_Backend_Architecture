d14af2d61aa14a9b3eba368ae33a163b7567e14f
LaH198 <haiche198@gmail.com>
2024-08-06 18:30:29 +0700

    Upload feature upload file to local and cloud + resize via cloudinary

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,12 +1,28 @@
 "use strict";
-import { uploadImageFromUrl } from "../services/upload.service.js";
+import {
+  uploadFileFromUrl,
+  uploadFileThumb,
+} from "../services/upload.service.js";
 
 import { SuccessResponse } from "../core/success.response.js";
+import { BadRequestError } from "../core/error.response.js";
 class UploadController {
-  uploadImageFromUrl = async (req, res, next) => {
+  uploadFileFromUrl = async (req, res, next) => {
     new SuccessResponse({
       message: "uploadImageFromUrl success",
-      metadata: await uploadImageFromUrl(),
+      metadata: await uploadFileFromUrl(),
+    }).send(res);
+  };
+  uploadFileThumb = async (req, res, next) => {
+    const { file } = req;
+    if (!file) {
+      throw new BadRequestError("File missing");
+    }
+    new SuccessResponse({
+      message: "uploadFileThumb success",
+      metadata: await uploadFileThumb({
+        path: file.path,
+      }),
     }).send(res);
   };
 }
--- src/routes/upload/index.js
+++ src/routes/upload/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import uploadController from "../../controllers/upload.controller.js";
 import { asyncHandler } from "../../helpers/asyncHandler.js";
+import { uploadDisk } from "../../configs/multer.config.js";
 // import { authenticationV2 } from "../../auth/authUtils.js";
 const router = express.Router();
 
 // router.use(authenticationV2); // authentication
 
-router.post("/product", asyncHandler(uploadController.uploadImageFromUrl));
+router.post("/product", asyncHandler(uploadController.uploadFileFromUrl));
+router.post(
+  "/product/thumb",
+  uploadDisk.single("file"),
+  asyncHandler(uploadController.uploadFileThumb)
+);
 
 export default router;
